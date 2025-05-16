1e9ab7332db5a7ea62a8458221168509292825b7
Aiden <ateal@ualberta.ca>
2025-05-16 11:00:36 -0600

    Setup Authentication Layouts, Setup Next Auth with Github and Google support

---
+++ devhuddle/constants/routes.ts
@@ -0,0 +1,10 @@
+// centralized place for defining routes in the application
+// If any routing changes need to be made, it can be directly done here
+
+const ROUTES = {
+    HOME: '/',
+    SIGN_IN: '/sign-in',
+    SIGN_UP: '/sign-up'
+};
+
+export default ROUTES;
